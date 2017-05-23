03ee0de28c27387fdf58f1db80edd8c1b4e94a49
dommomo <dommomo@gmail.com>
2017-05-23 12:21:07 +0930

    removed rigged camera and setup for new script

--- Assets/Game/GameManager.cs
+++ Assets/Game/GameManager.cs
@@ -7,6 +7,9 @@ public class GameManager : MonoBehaviour {
 
     public bool recording = true;
 
+    private bool paused = false;
+    private float timeScale, fixedDeltaTime;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -16,5 +19,34 @@ public class GameManager : MonoBehaviour {
     void Update()
     {
         recording = (!CrossPlatformInputManager.GetButton("Fire1"));
+
+        if (Input.GetKeyUp(KeyCode.P))
+        {
+            if (!paused) { PauseGame(); }
+            else { ResumeGame(); }
+        }
+    }
+
+    void PauseGame ()
+    {
+        this.timeScale = Time.timeScale;
+        this.fixedDeltaTime = Time.fixedDeltaTime;
+        Time.timeScale = 0;
+        Time.fixedDeltaTime = 0;
+        paused = true;
+    }
+
+    void ResumeGame ()
+    {
+        Time.timeScale = this.timeScale;
+        Time.fixedDeltaTime = this.fixedDeltaTime;
+        paused = false;
+    }
+
+    private void OnApplicationPause(bool pause)
+    {
+        if (!paused && pause) { PauseGame(); }
+
+        if (paused && !pause) { ResumeGame(); }
     }
 }
